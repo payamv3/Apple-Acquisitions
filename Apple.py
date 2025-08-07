f144022c4f9eb4f0b574d97a93ba23ec86f1d44c
Payam Saeedi <76733438+payamv3@users.noreply.github.com>
2025-08-07 17:01:10 -0400

    Update Apple.py

--- Apple.py
+++ Apple.py
@@ -48,11 +48,27 @@ bars = alt.Chart(source.reset_index()).mark_bar(cornerRadiusTopLeft=3,
 ).properties(title = 'Apple Acquisitions Through Time', width = 1200, height = 400)
 
 
-text = bars.mark_text(
+#text = bars.mark_text(
+#    align='center',
+#    baseline='middle' , dy = -6
+#).encode(
+#    text='Company:Q'
+#)
+
+text = alt.Chart(source.reset_index()).mark_text(
     align='center',
-    baseline='middle' , dy = -6
+    baseline='middle',
+    dy=-6
 ).encode(
-    text='Company:Q'
+    alt.X('Year:O'),
+    alt.Y('Company:Q'),
+    text='Company:Q',
+    # Apply the same color condition to the text
+    color=alt.condition(
+        alt.datum.Year > 2011,
+        alt.value('steelblue'),
+        alt.value('grey')
+    )
 )
 
 
